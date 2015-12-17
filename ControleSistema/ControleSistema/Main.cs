013a7cc7722a13fb404072eef8ae8500f8cbd9bc
Publyo (Taulim) <publyo@gmail.com>
2015-12-17 14:47:32 -0200

    Backup

---
+++ ControleSistema/ControleSistema/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ControleSistema
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            ScriptManager sm = new ScriptManager();
+            this.webBrowser1.ObjectForScripting = sm;
+        }
+    }
+}
---
+++ ControleSistema/ControleSistema/ScriptManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ControleSistema
+{
+    [System.Runtime.InteropServices.ComVisible(true)]
+    public class ScriptManager
+    {
+        public void Teste1()
+        {
+            System.Windows.Forms.MessageBox.Show("Teste 1");
+        }
+    }
+}
