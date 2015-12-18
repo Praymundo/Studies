85449e299baf074beb507f6f835c45b8f40fbe35
Publyo (Praymundo) <publyo@gmail.com>
2015-12-17 22:21:38 -0200

    Backup

---
+++ ImgCatch/ImgCatch/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace ImgCatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(48, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(861, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(12, 65);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(897, 164);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            this.webBrowser1.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser1_Navigated);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(753, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 235);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(897, 69);
+            this.listBox1.TabIndex = 3;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 3000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(834, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(48, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(699, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "URL:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Title:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(921, 320);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Img Catch";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ImgCatch/ImgCatch/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using mshtml;
+using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+namespace ImgCatch
+{
+    public partial class Form1 : Form
+    {
+        string lastURL = "";
+        string proxURL = "";
+        int count = 0;
+
+        private bool _Running;
+        public bool Running
+        {
+            get
+            {
+                return this._Running;
+            }
+            private set
+            {
+                this._Running = value;
+                button1.Enabled = !(this._Running);
+                textBox1.Enabled = !(this._Running);
+                textBox2.Enabled = !(this._Running);
+                button2.Enabled = (this._Running);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Running = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string urlInicial = "";
+            urlInicial = "";
+            if (urlInicial == "")
+            {
+                urlInicial = textBox1.Text;
+            }
+
+            Running = true;
+            webBrowser1.Navigate(urlInicial);
+        }
+
+        private int GetRandomInterval()
+        {
+            Random r = new Random();
+            return r.Next(2500, 5000);
+        }
+
+        private void InicioVerificarImagem()
+        {
+            timer1.Interval = GetRandomInterval();
+            timer1.Start();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (!Running) return;
+
+            timer1.Stop();
+            InicioVerificarImagem();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!Running) return;
+            string title = textBox2.Text;
+            if (title == "")
+            {
+                AdicionarLog("Titulo vazio");
+                return;
+            }
+
+            string path = Path.Combine(@"C:\", "Img", title);
+            DirectoryInfo dInfo = new DirectoryInfo(path);
+            if (!dInfo.Exists)
+            {
+                // Directory.CreateDirectory(path);
+                dInfo.Create();
+                dInfo.Refresh();
+                if (!dInfo.Exists)
+                {
+                    AdicionarLog("Erro ao criar pasta");
+                    return;
+                }
+
+                DirectorySecurity sec = dInfo.GetAccessControl();
+                // Using this instead of the "Everyone" string means we work on non-English systems.
+                SecurityIdentifier everyone = new SecurityIdentifier(WellKnownSidType.WorldSid, null);
+                sec.AddAccessRule(new FileSystemAccessRule(everyone, FileSystemRights.Modify | FileSystemRights.Synchronize, InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit, PropagationFlags.None, AccessControlType.Allow));
+                dInfo.SetAccessControl(sec);
+            }
+
+            IHTMLDocument2 htmlDoc2 = (IHTMLDocument2)webBrowser1.Document.DomDocument;
+
+            if (htmlDoc2.title == "Error")
+            {
+                AdicionarLog("Erro: " + proxURL);
+                timer1.Stop();
+                webBrowser1.Stop();
+                InicioNavegarProximo();
+                return;
+            }
+
+            HtmlElementCollection els = webBrowser1.Document.GetElementsByTagName("img");
+
+            foreach (HtmlElement el in els)
+            {
+                string className = el.GetAttribute("className");
+                if (className == "b")
+                {
+                    string urlImg = el.GetAttribute("href");
+                    if (lastURL != urlImg)
+                    {
+                        webBrowser1.Document.Body.ScrollIntoView(false);
+                        lastURL = urlImg;
+                        AdicionarLog(urlImg);
+                        timer1.Stop();
+                        webBrowser1.Stop();
+                        proxURL = el.Parent.GetAttribute("href");
+
+                        IHTMLControlRange imgRange = (IHTMLControlRange)((HTMLBody)htmlDoc2.body).createControlRange();
+                        imgRange.add((IHTMLControlElement)el.DomElement);
+                        imgRange.execCommand("Copy", false, null);
+                        using (Bitmap bmp = (Bitmap)Clipboard.GetDataObject().GetData(DataFormats.Bitmap))
+                        {
+                            bmp.Save(Path.Combine(path, ((IHTMLImgElement)el.DomElement).nameProp));
+                        }
+                        Clipboard.Clear();
+
+                        InicioNavegarProximo();
+                    }
+                }
+            }
+        }
+
+        private void InicioNavegarProximo()
+        {
+            timer2.Interval = GetRandomInterval();
+            timer2.Start();
+        }
+
+        private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            textBox1.Text = webBrowser1.Url.ToString();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (!Running) return;
+
+            timer2.Stop();
+            webBrowser1.Navigate(proxURL);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Running = false;
+            timer1.Stop();
+            timer2.Stop();
+            webBrowser1.Stop();
+            webBrowser1.Navigate("");
+        }
+
+        private void AdicionarLog(string pLog)
+        {
+            listBox1.Items.Add(pLog);
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (Running) return;
+
+            Uri url = new Uri(textBox1.Text);
+            string fileName = System.IO.Path.GetFileName(url.LocalPath);
+            string[] lInfo = fileName.Split('.');
+            string[] lTitle = lInfo[0].Split('_');
+            textBox2.Text = lTitle[0];
+            return;
+        }
+    }
+}
